bedc35204481cf4a98f9cf6cebaa97fd69aa9ae7
Vishram7 <124357319+Vishram7@users.noreply.github.com>
2024-08-30 10:55:33 +0530

    Update useRegister.jsx

--- src/hooks/useRegister.jsx
+++ src/hooks/useRegister.jsx
@@ -15,7 +15,7 @@ export const useRegister = () => {
         setIsLoading(true)
         setError(null)
 
-        const response = await fetch('http://localhost:3000/api/user/signup', {
+        const response = await fetch('https://auth-back2.onrender.com/api/user/signup', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({email, password})
